b8ebf0c6930f5936501e37063940949ac490ab4f
MAGAEGJF <jf.galindo@ibermatica.com>
2023-07-24 01:24:55 +0200

    24-07-Cambios realizados

--- actualizar_filtro_am_fullannual.py
+++ actualizar_filtro_am_fullannual.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import pandas as pd
-from openpyxl import load_workbook
+#from openpyxl import load_workbook
 from contextlib import nullcontext
 
 # General terminology translation
@@ -11,69 +11,108 @@ from contextlib import nullcontext
 #row-row
 #NaN - empty cell
 
+
+
+#Funcion copiado hojas excel con backup
+def cp_Excel_bck(rOrig, fOrig, shOrig, rDest, fDest, shDest): # noqa: E999
+    stError = False
+    resultado = ""
+    fBackup = fDest + '_v1'
+    #1 paso) Backup del fichero filtro de origen
+    try:
+        src_file = os.path.join(rDest, fDest)   #Fichero origen
+        dst_file = os.path.join(rDest, fBackup)    #Fichero destino
+        if not os.path.exists(src_file):
+            print("Los ficheros origen o destino, no existen")
+            stError = True
+            resultado = "Error el fichero origen o destino no existen"
+        else: 
+            shutil.copy(src_file, dst_file)
+            resultado = "Backup filtro realizado"
+            print(resultado)
+            stError = False
+    except Exception as e:
+        print(f"How exceptional! {e}")
+        print("Error, no se pudo copiar el archivo de backup")
+        stError = True
+    
+    #1.bis. Fichero origen
+    print("Fichero origen: ", fOrig)
+    src_file = os.path.join(rOrig, fOrig)
+    if not os.path.exists(src_file):
+        print("Error, el fichero origen no existe", src_file)
+        stError = True
+    else:
+        fBackup = "Filtro_origen_backup_v1"
+        dst_file = os.path.join(rOrig, fBackup)
+        try:
+            shutil.copy(src_file, dst_file)
+            resultado = "Backup origen realizado"
+            print(resultado)
+            stError = False
+        except Exception as e:
+            print(f"How exceptional! {e}")
+            print("Error copia fichero origen")
+            stError = True
+
+    #2 paso) Obtener el fichero origen
+
+    if fOrig is not nullcontext and shOrig is not nullcontext and not stError:
+        print("Fichero origen: ", fOrig)
+        src_file = os.path.join(rOrig, fOrig)
+        if not os.path.exists(src_file):
+            print("Error, el fichero origen no existe", src_file)
+            resultado = "Error el fichero origen no existe"
+            stError = True
+        else:
+            # Load Excel File and give path to your file
+            try:
+                df_Filtro = pd.read_excel(src_file, sheet_name= shOrig)
+                resultado = "Carga archivo origen"
+                stError = False
+                print(resultado + ' ' + fOrig)
+                
+            except Exception as e:
+                print(f"How exceptional! {e}")
+                print("Error, no se pudo abrir el fichero origen", src_file)
+                resultado = "Error carga archivo origen"
+                stError = True
+    else:
+        resultado = "Nombre vacio fichero origen"
+        stError = True
+
+    #3 paso) Escritura del df en un archivo excel
+    if not stError:
+        try:
+            dst_file = os.path.join(rDest, fDest)
+    
+            df_Filtro.to_excel(dst_file, sheet_name = shDest, index=False)
+            resultado = "Escribe archivo destino"
+            stError = False
+            print(resultado + ' ' + dst_file)
+        except Exception as e:
+            print(f"How exceptional! {e}")
+            print("Error, no se pudo grabar el archivo destino")
+            resultado = "Error escritura archivo destino"
+            stError = True
+
+    #4 paso) Resultado
+        return stError
+
+
+#Proceso principal
 # Give the location of the file
 My_downloads = "C:\\Users\\magaegjf\\Downloads\\"
 My_Tron_path = "C:\\Users\\magaegjf\\OneDrive - AYESA\\SVC-MAPFRE-TRON\\01.GESTION\\1.PRODUCCION\\2.Avances\\CdM"
-fTarget_filter_file = "TRON.AM.IBERMATICA.FULLANNUAL.xlsx"
-fTarget_filter_backup ="TRON.AM.IBERMATICA.FULLANNUAL_v1.xlsx"
-resultado = ""
-stError = False
-
-#datos_origen = pd.read_excel(fOrigen)
-#sales_data = pd.read_excel(fOrigen)
-
-#Backup del fichero filtro de origen
-try:
-    src_file = os.path.join(My_Tron_path, fTarget_filter_file)   #Fichero origen
-    dst_file = os.path.join(My_Tron_path, fTarget_filter_backup) #Fichero destino
-    shutil.copy(src_file, dst_file)
-    resultado = "Backup filtro realizado"
-    print(resultado)
-    stError = False
-except:
-    print("Error, no se pudo copiar el archivo de backup")
-    stError = True
-
-# Parsing archivo lectura
-#pd.read_excel("path_to_file.xls", "Sheet1", index_col=None, na_values=["NA"])
-
+fTarget_file = "TRON.AM.IBERMATICA.FULLANNUAL.xlsx"
+fTarget_backup ="TRON.AM.IBERMATICA.FULLANNUAL_v1.xlsx"
 fNombreOrigen = str(input("Introduzca nombre fichero origen: "))
-if fNombreOrigen is not nullcontext:
-    fOrigen = My_downloads + fNombreOrigen + ".xlsx"
-    print("Fichero origen: ", fOrigen)
-    if not os.path.exists(fOrigen):
-        print("Error, el fichero origen no existe")
-        resultado = "Error el fichero origen no existe"
-        stError = True
-    else:
-        # Load Excel File and give path to your file
-        try:
-            src_hoja = "Your Jira Issues"
-            df_Filtro = pd.read_excel(fOrigen, sheet_name= src_hoja)
-            resultado = "Carga archivo origen"
-            stError = False
-            print(resultado + ' ' + fOrigen)
-            m_row = df_Filtro.max_row
-            print("Filas leidas: ", m_row)
-        except:
-            print("Error, no se pudo abrir el fichero origen")
-            resultado = "Error carga archivo origen"
-            stError = True
-else:
-    resultado = "Nombre vacio fichero origen"
-    stError = True
+fSrcOrigen = My_downloads + fNombreOrigen + ".xlsx"
+src_hoja = "Your Jira Issues"
+dst_hoja = "TRON.FULLANNUAL"
 
-# Escritura del df en un archivo excel
-try:
-    dst_file = os.path.join(My_Tron_path, fTarget_filter_file)
-    dst_hoja = "TRON.FULLANNUAL"
-    df_Filtro.to_excel(dst_file, sheet_name = dst_hoja, index=False)
-    resultado = "Escribe archivo destino"
-    stError = False
-    print(resultado + ' ' + dst_file)
-except:
-    print("Error, no se pudo grabar el archivo destino")
-    resultado = "Error escritura archivo destino"
-    stError = True
-
-
+stResultado = False
+stResultado = cp_Excel_bck(rOrig = My_downloads, fOrig = fSrcOrigen, 
+                            shOrig = src_hoja, rDest = My_Tron_path, 
+                            fDest = fTarget_file, shDest = dst_hoja)
+print(f'Resultado de copiar_planillas_backup: stResultado = {stResultado}')
